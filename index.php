0ce9e6d2916e98b0ab770ec92b0dd26c73efb5a4
David Berrospe Ramírez <david.berrospe.r03@gmail.com>
2021-06-27 15:32:48 -0500

    Added JQuery.

--- index.php
+++ index.php
@@ -24,6 +24,7 @@
 		</div>
 	</div>
 
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script type="text/javascript" src="./js/index.js"></script>
 </body>
 </html>
